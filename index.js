ae50be88b94aeed5308cc109fd7c88b607c2ddf5
PeaFunk3000 <80793315+PeaFunk3000@users.noreply.github.com>
2023-02-21 20:14:34 +0000

    all prompt questions complete and runs in command line

--- index.js
+++ index.js
@@ -9,4 +9,49 @@ inquirer
       message: "What is the title of your project?",
       name: "title",
     },
+    {
+      type: "input",
+      message: "Repository directory name",
+      name: "repo",
+    },
+    {
+      type: "input",
+      message: "Project description",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "Installation instructions?",
+      name: "ins",
+    },
+    {
+      type: "input",
+      message: "Usage instructions?",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "Any project test requirements?",
+      name: "tests",
+    },
+    {
+      type: "input",
+      message: "Any project contributors or credits?",
+      name: "credits",
+    },
+    {
+      type: "list",
+      message: "Which license type does your project use?",
+      name: "license",
+      choices: ["MIT", "IBM", "Mozilla", "Perl", "Eclipse", "ISC", "none"],
+    },
+    {
+      type: "input",
+      message: "What is your contact email address?",
+      name: "contact",
+    },   {
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "user",
+    }
 ])
