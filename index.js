971caffe8eb894ac59b6fcdcfa436f191c9c3caa
PeaFunk3000 <80793315+PeaFunk3000@users.noreply.github.com>
2023-02-21 20:28:15 +0000

    readme template literal completed - license badge still to do

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ inquirer
   .prompt([
     {
       type: "input",
-      message: "What is the title of your project?",
+      message: "Project title",
       name: "title",
     },
     {
@@ -21,37 +21,37 @@ inquirer
     },
     {
       type: "input",
-      message: "Installation instructions?",
+      message: "Installation instructions",
       name: "ins",
     },
     {
       type: "input",
-      message: "Usage instructions?",
+      message: "Usage instructions",
       name: "usage",
     },
     {
       type: "input",
-      message: "Any project test requirements?",
+      message: "Project test requirements",
       name: "tests",
     },
     {
       type: "input",
-      message: "Any project contributors or credits?",
+      message: "Project contributors or credits",
       name: "credits",
     },
     {
       type: "list",
-      message: "Which license type does your project use?",
+      message: "Select license",
       name: "license",
       choices: ["MIT", "IBM", "Mozilla", "Perl", "Eclipse", "ISC", "none"],
     },
     {
       type: "input",
-      message: "What is your contact email address?",
+      message: "Contact email address",
       name: "contact",
     },   {
       type: "input",
-      message: "What is your GitHub username?",
+      message: "GitHub username",
       name: "user",
     }
 ])
@@ -78,7 +78,40 @@ function writeDoc(response) {
   - [Tests](#tests)
   - [Contributing](#contributing)
   - [Lisence](#license)
-  - [Questions](#questions)`;
+  - [Questions](#questions)
+  
+  
+${response.license !== "none" ? `- [License](#license)` : ""}
+
+## Installation
+
+${response.ins}
+
+## Usage
+
+${response.usage}
+
+The repository for this project can be found on my GitHub:
+
+https://github.com/${response.user}/${response.repo}
+
+## Tests
+
+${response.tests}
+
+## Contributing
+
+${response.credits}
+
+${response.license !== "none" ? `## License` : ""}
+
+## Questions
+
+* For any additional questions regarding this project, please contact me on:
+${response.contact}
+
+* A link to my GitHub profile:
+https://github.com/${response.user}`;
 
   fs.writeFile("yourReadme.md", myReadme, (err) => {
     if (err) console.log(err);
